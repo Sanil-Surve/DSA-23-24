e1186fc2637cf0ce42c84228f3cdb41ad41c6cb0
Sanil-Surve <sanilsurve42@gmail.com>
2023-12-16 19:02:19 +0530

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int age;
+    string name;
+
+    cin >> age;
+    cin >> name;
+    cout << "My Name is " << name << " and Age is " << age << endl;
+    return 0;
+}
