2d8b09c59d55831214cf6bac8403c511bb3d85c4
Sanil-Surve <sanilsurve42@gmail.com>
2023-12-16 19:24:04 +0530

    initial commit

---
+++ index.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+    int a, b, c;
+    a = 20;
+    b = 30;
+    c = a + b;
+    cout << "the sum of a and b is " << c;
+    return 0;
+}
