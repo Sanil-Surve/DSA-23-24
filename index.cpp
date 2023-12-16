e7ab5a31974f2211b40fb4e9b1482c175c42888e
Sanil-Surve <sanilsurve42@gmail.com>
2023-12-16 19:26:43 +0530

    changed index.cpp

--- index.cpp
+++ index.cpp
@@ -5,8 +5,8 @@ using namespace std;
 int main()
 {
     int a, b, c;
-    a = 20;
-    b = 30;
+    cin >> a;
+    cin >> b;
     c = a + b;
     cout << "the sum of a and b is " << c;
     return 0;
